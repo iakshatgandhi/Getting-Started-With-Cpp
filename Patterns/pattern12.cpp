bed400ec32684b425193e75c62894da607618a59
Akshat Gandhi <akshat.gandhi224663@gmail.com>
2022-10-17 22:19:23 +0530

    17/10/22

---
+++ Patterns/pattern12.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+int i,j,n;
+cin>>n;
+i=1;
+while(i<=n){
+    j=1;
+    while(j<=n){
+        cout<<<<" ";
+        j++;
+    }
+    cout<<endl;
+    i++;
+}
+}
