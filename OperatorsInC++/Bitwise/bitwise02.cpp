1a5f020d9fbde527b71c6973a72e9fd5ca9f8ace
Akshat Gandhi <akshat.gandhi224663@gmail.com>
2022-11-09 19:26:24 +0530

    9/11/22

---
+++ OperatorsInC++/Bitwise/bitwise02.cpp
@@ -0,0 +1,4 @@
+// left shift operator left shifts the bits generally left shifting the integer value makes it the double of itself but thats the case only for the the small numbers if we consider large number in that case the if there is a one next to the MSB the on left shifting it comes over MSB and the number becomes negative.
+// Right shift operator shifts the bit towards right also right shifting the number generally halfs the number
+// when left and right shift is performed on the positive number then the padding is given 0 but in the case of the negative number its compilor dependent
+
