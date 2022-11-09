e988e0a8232b2b5916fe147accb3cf8f825f1cbf
Akshat Gandhi <akshat.gandhi224663@gmail.com>
2022-11-09 19:53:21 +0530

    09/11/22

--- OperatorsInC++/Bitwise/bitwise02.cpp
+++ OperatorsInC++/Bitwise/bitwise02.cpp
@@ -2,3 +2,13 @@
 // Right shift operator shifts the bit towards right also right shifting the number generally halfs the number
 // when left and right shift is performed on the positive number then the padding is given 0 but in the case of the negative number its compilor dependent
 
+#include<iostream>
+using namespace std;
+int main(){
+   int a=17;
+   int b=19;
+    cout<<(a>>1)<<endl;
+    cout<<(a>>2)<<endl;
+    cout<<(b<<1)<<endl;
+    cout<<(21<<2);
+}
