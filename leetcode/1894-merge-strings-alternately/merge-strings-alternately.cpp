ed5ef8dda512d7811361e0973e121b7f1bdc069e
Akshat Gandhi <Akshat.21SCSE1011212@galgotiasuniversity.edu.in>
2025-07-28 02:08:47 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (31.41%) - LeetSync

--- leetcode/1894-merge-strings-alternately/merge-strings-alternately.cpp
+++ leetcode/1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -3,19 +3,14 @@ public:
     string mergeAlternately(string word1, string word2) {
         string result;
         int i=0, j=0;
-        while(i<word1.length() && i<word2.length()){
+        while(i<word1.length() && j<word2.length()){
             result.push_back(word1[i]);
-            result.push_back(word2[i]);
+            result.push_back(word2[j]);
             i++;
+            j++;
         }
-        while(i<word1.length()){
-            result.push_back(word1[i]);
-            i++;
-        }
-        while(i<word2.length()){
-            result.push_back(word2[i]);
-            i++;
-        }
+        result += word1.substr(i);
+        result += word2.substr(j);
         return result;
     }
 };
