68ee5e815d0018c6d384f73841aa4288fe28dd98
Akshat Gandhi <Akshat.21SCSE1011212@galgotiasuniversity.edu.in>
2025-07-29 01:52:47 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (31.44%) - LeetSync

--- leetcode/1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
+++ leetcode/1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
-    bool division(string s, string t){
-        if(s.length() % t.length() != 0) return false;
-
-        string repeated;
-        for (int i = 0; i < s.size() / t.size(); ++i)
-            repeated += t;
-        return repeated == s;
-
+    int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
     }
     string gcdOfStrings(string str1, string str2) {
-        string result;
-        int minlen = min(str1.length(), str2.length());
-        for(int i=1;i<=minlen;i++){
-            string candidate = str1.substr(0,i);
-            if(division(str1,candidate) && division(str2,candidate)){
-                result = candidate;
-            }
-        }
-        return result;
+        if (str1 + str2 != str2 + str1) return "";
+        int len = gcd(str1.size(), str2.size());
+        return str1.substr(0, len);
     }
 };
+
+
+
+
+//If a string x repeats m times to form str1 and n times to form str2,
+
+//Then x’s length is the GCD of the lengths of str1 and str2.
+
+// Using __gcd from <numeric> (C++17+):
