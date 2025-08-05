4898fd0a0cbc720a8e02634ae7ec7cada1d29aa7
Akshat Gandhi <Akshat.21SCSE1011212@galgotiasuniversity.edu.in>
2025-08-06 00:22:52 +0530

    Time: 54 ms (13.58%) | Memory: 75.2 MB (31.93%) - LeetSync

--- leetcode/940-fruit-into-baskets/fruit-into-baskets.cpp
+++ leetcode/940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        unordered_map<int, int> basket;
-        int left = 0, maxFruits = 0;
-        for (int right = 0; right < fruits.size(); ++right) {
+        unordered_map<int,int> basket;
+        int left = 0;
+        int right = 0;
+        int maxPicked = 0;
+        while(right<fruits.size()){
             basket[fruits[right]]++;
-            while (basket.size() > 2) {
+            if(basket.size()<=2){
+                maxPicked = max(maxPicked,right-left+1);
+            }else{
                 basket[fruits[left]]--;
-                if (basket[fruits[left]] == 0)
-                    basket.erase(fruits[left]);
-                ++left;
+                if(basket[fruits[left]]==0) basket.erase(fruits[left]);
+                left++;
             }
-            maxFruits = max(maxFruits, right - left + 1);
+            right++;
         }
-        return maxFruits;
+        return maxPicked;
     }
 };
