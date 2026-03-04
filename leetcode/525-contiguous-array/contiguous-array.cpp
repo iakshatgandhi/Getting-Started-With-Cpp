7288f3fbfa9677e814e05164a9f70ac49d051830
Akshat Gandhi <Akshat.21SCSE1011212@galgotiasuniversity.edu.in>
2026-03-04 22:22:37 +0530

    Time: 29 ms (27.37%) | Memory: 88.2 MB (92.35%) - LeetSync

--- leetcode/525-contiguous-array/contiguous-array.cpp
+++ leetcode/525-contiguous-array/contiguous-array.cpp
@@ -3,18 +3,14 @@ public:
     int findMaxLength(vector<int>& nums) {
         unordered_map<int,int> prevIdx;
         prevIdx[0]=-1;
-        int currSum = 0;
-        int maxSize = 0;
+        int currSum =0;
+        int maxLen = 0;
         for(int i=0;i<nums.size();i++){
-            if(nums[i]==0) currSum-=1;
-            else currSum+=nums[i];
-
+            (nums[i]==0)?currSum-=1:currSum+=nums[i];
             if(prevIdx.find(currSum)!=prevIdx.end()){
-                maxSize=max(maxSize,i-prevIdx[currSum]);
-            }else {
-                prevIdx[currSum] = i;
-            }
+                maxLen = max(maxLen,i-prevIdx[currSum]);
+            }else prevIdx[currSum]=i;
         }
-        return maxSize;
+        return maxLen;
     }
 };
