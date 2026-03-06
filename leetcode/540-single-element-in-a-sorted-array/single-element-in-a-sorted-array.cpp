3ebec0601943153745bccd4c4dac58ebf087c598
Akshat Gandhi <Akshat.21SCSE1011212@galgotiasuniversity.edu.in>
2026-03-06 22:53:10 +0530

    Time: 0 ms (100.00%) | Memory: 26.3 MB (34.40%) - LeetSync

--- leetcode/540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ leetcode/540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int n = nums.size();
-        if(n == 1) return nums[0];
-        for(int i=0;i<n;i++){
-            if(i==0){
-                if(nums[i+1]!=nums[i])return nums[i];
-            }else if(i==n-1){
-                if(nums[i]!=nums[i-1]) return nums[i];
-            }else{
-                if(nums[i]!= nums[i+1] && nums[i] != nums[i-1]){
-                    return nums[i];
-                }
+        int n=nums.size();
+        if(n==1) return nums[0];
+        if(nums[0]!=nums[1]) return nums[0];
+        if(nums[n-1]!=nums[n-2]) return nums[n-1];
+        int start = 1;
+        int end = n-2;
+        int mid = start+(end-start)/2;
+        while(start<=end){
+            if(nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1]){
+                return nums[mid];
             }
+            
+            if(mid % 2==0 && nums[mid]==nums[mid+1] || mid % 2==1 && nums[mid]==nums[mid-1]){
+                start = mid+1;
+            }else end = mid-1;
+            mid = start+(end-start)/2;
         }
         return -1;
     }
