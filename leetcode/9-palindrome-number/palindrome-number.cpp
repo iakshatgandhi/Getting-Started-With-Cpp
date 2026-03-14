56a1722fd5e9966f4251793ee0c8fef097df9547
Akshat Gandhi <Akshat.21SCSE1011212@galgotiasuniversity.edu.in>
2026-03-15 00:40:36 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (37.47%) - LeetSync

--- leetcode/9-palindrome-number/palindrome-number.cpp
+++ leetcode/9-palindrome-number/palindrome-number.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        string s = to_string(x);
-        int l = 0,r = s.length()-1;
-        while(l<r){
-            if(s[l++]!=s[r--])return 0;
+        if(x<0 || (x%10==0 && x!=0)) return 0;
+        int halfRev = 0;
+        while(x>halfRev){
+            halfRev = halfRev * 10 + (x%10);
+            x /= 10;
         }
-        return 1;
+        return halfRev == x || halfRev/10 == x;
     }
 };
