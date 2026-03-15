1abf05088d5b42efa873f65db11ff0090af56454
Akshat Gandhi <Akshat.21SCSE1011212@galgotiasuniversity.edu.in>
2026-03-16 00:11:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (44.57%) - LeetSync

--- leetcode/258-add-digits/add-digits.cpp
+++ leetcode/258-add-digits/add-digits.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
-    int addDigits(int num) {
-        if (num == 0) return 0;
-        return (num % 9 == 0) ? 9 : num % 9;
+    int addDigits(int num) { 
+        return 1 + (num - 1) % 9;;
     }
 };
 
