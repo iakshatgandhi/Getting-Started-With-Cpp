af061fea10aea0f2d0bc5ce0afeaacea21644e6f
Akshat Gandhi <Akshat.21SCSE1011212@galgotiasuniversity.edu.in>
2026-03-17 00:15:31 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (17.39%) - LeetSync

--- leetcode/367-valid-perfect-square/valid-perfect-square.cpp
+++ leetcode/367-valid-perfect-square/valid-perfect-square.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     bool isPerfectSquare(int num) {
-       double ans=sqrt(num);
-       if(ans == (int)ans) return 1;
-        return 0;
+        long long root = sqrt(num);
+        return root * root == num;
     }
 };
