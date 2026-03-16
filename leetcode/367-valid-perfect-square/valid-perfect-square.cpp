7bbc4131192d0a504f66cce34657601e5fdc04d9
Akshat Gandhi <Akshat.21SCSE1011212@galgotiasuniversity.edu.in>
2026-03-17 00:35:24 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (86.88%) - LeetSync

--- leetcode/367-valid-perfect-square/valid-perfect-square.cpp
+++ leetcode/367-valid-perfect-square/valid-perfect-square.cpp
@@ -1,7 +1,16 @@
 class Solution {
 public:
     bool isPerfectSquare(int num) {
-        long long root = sqrt(num);
-        return root * root == num;
+        if(num==1)return 1;
+        int low = 0;
+        int high = num/2;
+        while(low<=high){
+            long long mid = low + (high-low)/2;
+            long long sqrt = mid * mid;
+            if(sqrt == num) return 1;
+            else if(sqrt<num) low=mid+1;
+            else high=mid-1;
+        }
+        return 0;
     }
 };
