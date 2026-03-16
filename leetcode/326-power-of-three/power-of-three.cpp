e33c0d562e522c3769084da3bf3312b012811b22
Akshat Gandhi <Akshat.21SCSE1011212@galgotiasuniversity.edu.in>
2026-03-16 18:48:58 +0530

    Time: 2 ms (64.93%) | Memory: 8.8 MB (74.26%) - LeetSync

--- leetcode/326-power-of-three/power-of-three.cpp
+++ leetcode/326-power-of-three/power-of-three.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     bool isPowerOfThree(int n) {
         if(n<=0)return 0;
-        double x = log10(n)/log10(3);
-        return x==(int)x;
+        if(n==1) return 1;
+
+        return n%3==0 && isPowerOfThree(n/3);
     }
 };
