969681a87b551b4adcf087fc369d92703cbac6a4
Akshat Gandhi <Akshat.21SCSE1011212@galgotiasuniversity.edu.in>
2026-03-17 14:11:21 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (87.30%) - LeetSync

--- leetcode/745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
+++ leetcode/745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
@@ -1,9 +1,17 @@
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
-        for(int i=0;i<letters.size();i++){
-            if(letters[i]-'a'>target-'a') return letters[i];
+        int n = letters.size();
+        int s = 0;
+        int e = n-1;
+        int idx = 0;
+        while(s<=e){
+            int m = s+(e-s)/2;
+            if(letters[m]-'a'>target-'a'){
+                idx = m;
+                e = m-1;
+            } else s=m+1;
         }
-        return letters[0];
+        return letters[idx];
     }
 };
