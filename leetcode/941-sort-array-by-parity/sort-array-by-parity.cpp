50971a5ed1496d9950b42115cf14490b21b6bfd9
Akshat Gandhi <Akshat.21SCSE1011212@galgotiasuniversity.edu.in>
2026-03-28 21:38:11 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (43.57%) - LeetSync

---
+++ leetcode/941-sort-array-by-parity/sort-array-by-parity.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        int i = 0, j = nums.size()-1;
+        while(i<j){
+            while(i<j && nums[i]%2==0) i++;
+            while(i<j && nums[j]%2!=0) j--;
+            if(i<j){
+                swap(nums[j--],nums[i++]);
+            }
+        }
+        return nums;
+    }
+};
