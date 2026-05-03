4be523f735261068d3a3d57ac2a98d039dd36902
Akshat Gandhi <Akshat.21SCSE1011212@galgotiasuniversity.edu.in>
2026-05-03 18:52:21 +0530

    Time: 7 ms (99.31%) | Memory: 50.4 MB (75.54%) - LeetSync

--- leetcode/208-implement-trie-prefix-tree/implement-trie-prefix-tree.cpp
+++ leetcode/208-implement-trie-prefix-tree/implement-trie-prefix-tree.cpp
@@ -19,38 +19,27 @@ public:
     Trie() {
         root = new trieNode('\0');
     }
-    
-    void insertRecur(trieNode* current, string word, int counter){
-        if(counter == word.length()){
-            current->isTerminal = true;
-            return;
-        }
-        int index = word[counter] - 'a';
-        if(current->children[index] == NULL){
-            current->children[index] = new trieNode(word[counter]);
-        }
-        insertRecur(current->children[index], word, counter+1);
-    }
 
     void insert(string word) {
-        int i=0;
-        return insertRecur(root,word,i);
-    }
-    
-    bool searchRecur(trieNode* current, string word, int counter){
-        if(counter == word.length()){
-            return current->isTerminal;
-        }
-        int index = word[counter] - 'a';
-        if(current -> children[index] == NULL){
-            return false;
-        }
-        return searchRecur(current -> children[index], word, counter+1);
+       trieNode* current = root;
+       for(char c : word){
+            int index = c-'a';
+            if(current -> children[index] == NULL){
+                current -> children[index] = new trieNode(c);
+            }
+            current = current -> children[index];
+       }
+       current -> isTerminal = true;
     }
 
     bool search(string word) {
-        int i=0;
-        return searchRecur(root, word, i);
+       trieNode* current = root;
+       for(char c : word){
+            int index = c-'a';
+            if(current -> children[index] == NULL) return 0;
+            current = current->children[index];
+       }
+       return current->isTerminal;
     }
     
     bool startsWith(string prefix) {
