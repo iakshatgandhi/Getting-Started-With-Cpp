6406da76aabaa5f5617e13a8789b94f08f2ed20f
Akshat Gandhi <Akshat.21SCSE1011212@galgotiasuniversity.edu.in>
2026-05-13 00:33:57 +0530

    Time: 15 ms (29.25%) | Memory: 34.5 MB (5.29%) - LeetSync

--- leetcode/2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.cpp
+++ leetcode/2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.cpp
@@ -1,24 +1,66 @@
+class trieNode {
+public:
+    trieNode* children[26];
+    bool isEndOf = false;
+
+    trieNode(){
+        for(int i=0;i<26;i++){
+            children[i] = NULL;
+        }
+        isEndOf = false;
+    }
+};
 class Solution {
 public:
-    vector<string> twoEditWords(vector<string>& queries, vector<string>& dictionary) {
-        vector<string> result;
-        for (const string& query : queries) {
-            bool found = false;
-            for (const string& word : dictionary) {
-                int diff = 0;
+    trieNode* root = new trieNode();
 
-                for (int i = 0; i < query.size(); i++) {
-                    if (query[i] != word[i]) diff++;
-                    if (diff > 2) break; // early exit
+    void insert(string& word){
+        trieNode* node = root;
+        for (char ch : word) {
+            int idx = ch-'a';
+            if(node->children[idx]==NULL){
+                node -> children[idx] = new trieNode();
+            }
+            node = node->children[idx];
+        }
+        node->isEndOf = true;
+    }
+
+    bool dfs(trieNode* node, string& query, int idx, int misMatched){
+        if(misMatched>2){
+            return false;
+        }
+        if(idx == query.size()){
+            return node->isEndOf;
+        }
+
+        int currentCar = query[idx]-'a';
+        for(int i=0;i<26;i++){
+            if(node->children[i]!=NULL){
+                int newMismatch = misMatched;
+                if(i != currentCar){
+                    newMismatch++;
                 }
-
-                if (diff <= 2) {
-                    found = true;
-                    break; // no need to check more dictionary words
+                if(dfs(node->children[i],query,idx+1,newMismatch)){
+                    return true;
                 }
             }
-            if (found) result.push_back(query);
         }
+        return false;
+    }
+    vector<string> twoEditWords(vector<string>& queries, vector<string>& dictionary) {
+        for (string& word : dictionary) {
+            insert(word);
+        }
+
+        vector<string> result;
+        for (string& query : queries) {
+
+            if (dfs(root, query, 0, 0)) {
+                result.push_back(query);
+            }
+        }
+
         return result;
     }
 };
